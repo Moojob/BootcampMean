e5aadde69f6142b12cfc36eb70c84382fbd08808
Moussa Diop <moudiop123@gmail.com>
2021-11-02 10:26:20 +0000

    mm

--- iteration.js
+++ iteration.js
@@ -1,4 +1,12 @@
+a=5;
 cities = ["Tunis","Tripoli","Alger","Rabat","Noakshot","Caire"]
+
 for(var i=0;i<cities.length;i++){
     console.log(cities[i]);
+    // i=i+1;
+}
+
+console.log('*-------------------------------------*')
+for(city of cities ){
+    console.log(city)
 }
